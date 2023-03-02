6c321b1cb4cab1822d106da7aadcb9169c8300b8
4190 <lukasz.dab4@gmail.com>
2023-03-02 17:19:47 +0100

    disabled market because of critical error in engine, added /addmount talkaction
    Signed-off-by: 4190 <lukasz.dab4@gmail.com>

--- data-otservbr-global/scripts/actions/object/market.lua
+++ data-otservbr-global/scripts/actions/object/market.lua
@@ -1,8 +1,8 @@
 local market = Action()
 
 function market.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	player:openMarket()
-	return true
+	--player:openMarket()
+	--return true
 end
 
 market:id(12903)
